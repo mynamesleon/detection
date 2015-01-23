ec26c02940787de5f388f23fcd4f71cd9d0898ae
mynamesleon <leon_slater@hotmail.co.uk>
2015-01-23 20:48:22 +0000

    small format update

--- client.js
+++ client.js
@@ -3,7 +3,7 @@
 ~~           Leon Slater, www.lpslater.co.uk           ~~
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
 
-window.client = (function(){
+window.client = (function () {
     var navAgent = navigator.userAgent.toLowerCase(),
         div = document.createElement('div'),
         img = new Image(),
@@ -30,7 +30,7 @@ window.client = (function(){
             mac: 'mac'
         },
         propChecks = { // css properties to check against 
-            perspective: ['perspective', 'WebkitPerspective', 'MozPerspective', 'OPerspective', 'msPerspective'], // check for translateZ and translate3d support
+            perspective: ['perspective', 'WebkitPerspective', 'MozPerspective', 'OPerspective', 'msPerspective'], // translateZ support
             transform: ['transform', 'WebkitTransform', 'MozTransform', 'OTransform', 'msTransform'],
             transition: ['transition', 'WebkitTransition', 'MozTransition', 'OTransition', 'msTransition'],
             willChange: ['willChange'],
@@ -46,71 +46,84 @@ window.client = (function(){
             requestAnimFrame: window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame,
             cancelAnimFrame: window.cancelAnimationFrame || window.webkitCancelRequestAnimationFrame || window.mozCancelRequestAnimationFrame
         },
-        uaCheck = function(val){
+        uaCheck = function (val) {
             var expr = new RegExp(val, ['i']);
             return expr.test(navAgent);
         },
-        cssCheck = function(props, objProp){
-            if (typeof props == 'string'){ // if a string is passed in, separate it into an array first
+        cssCheck = function (props, objProp) {
+            if (typeof props === 'string') { // if a string is passed in, separate it into an array first
                 props = props.split(' ');
             }
             for (var i in props) {
                 var isCalc = objProp === 'calc';
-                if (isCalc){ 
-                    div.style.cssText = 'width:' + props[i] + '(1px);'; 
+                if (isCalc) {
+                    div.style.cssText = 'width:' + props[i] + '(1px);';
+                    if (div.style.length) {
+                        return props[i];
+                    }
+                    continue;
                 }
-                if (isCalc ? div.style.length : div.style[props[i]] !== undefined) {                        
+                if (div.style[props[i]] !== undefined) {
                     return props[i];
                 }
             }
             return false;
-        };
-        addToClassStr = function(check, classToAdd){
-            if (check){
+        },
+        addToClassStr = function (check, classToAdd) {
+            if (check) {
                 classToAdd = classToAdd.toLowerCase();
-                if (html.className.indexOf(classToAdd) === -1){ 
+                if (html.className.indexOf(classToAdd) === -1) {
                     classStr += ' ' + classToAdd;
-                }   
+                }
             }
+        },
+        funcsToExpose = function () { // functions to expose in the client object
+            // polyfill for requestAnimationFrame and cancelAnimationFrame - adapted from original by Erik Moller
+            if ((!returnVals.requestAnimFrame) || (!returnVals.cancelAnimFrame)) {
+                var lastTime = 0;
+                returnVals.requestAnimFrame = function (callback, element) {
+                    var currTime = new Date().getTime(),
+                        timeToCall = Math.max(0, 16 - (currTime - lastTime)),
+                        id = window.setTimeout(function () { callback(currTime + timeToCall); }, timeToCall);
+                    lastTime = currTime + timeToCall;
+                    return id;
+                };
+                returnVals.cancelAnimFrame = function (id) {
+                    clearTimeout(id);
+                };
+            }
+            
+            // requires an array or space delimited string of css values: e.g. ['boxSizing', 'WebkitBoxSizing'] or 'boxSizing WebkitBoxSizing'
+            // returns the first supported value in the sequence 
+            returnVals.cssCheck = function (valueArray) {
+                return cssCheck(valueArray);
+            };
+
+            // takes a string (which will be converted into a regular expression) to check against the userAgent 
+            // e.g. 'firefox|chrome' will return true in both firefox and chrome
+            returnVals.uaCheck = function (userAgentValue) {
+                return uaCheck(userAgentValue);
+            };
         };
 
     // cycle through uaChecks and see if they're contained in the userAgent string
-    for (var s in uaChecks){ 
+    for (var s in uaChecks) {
         returnVals[s] = uaCheck(uaChecks[s]);
     }
 
     // create class string for checks so far
-    for (var u in returnVals){
+    for (var u in returnVals) {
         addToClassStr(returnVals[u], u); // add prop name if supported
     }
 
     // cycle through css property checks
-    for (var p in propChecks){
+    for (var p in propChecks) {
         returnVals[p] = cssCheck(propChecks[p], p);
         addToClassStr(returnVals[p], returnVals[p]); // add supported value to class string rather than prop name
     }
 
+    funcsToExpose();
     html.className += classStr;
-
-    // functions to expose in the client object
-    if ( (!returnVals.requestAnimFrame) || (!returnVals.cancelAnimFrame)  ){ // polyfill for requestAnimationFrame - adapted from original by Erik Moller
-        var lastTime = 0;
-        returnVals.requestAnimFrame = function(callback, element){
-            var currTime = new Date().getTime(),
-                timeToCall = Math.max(0, 16 - (currTime - lastTime)),
-                id = window.setTimeout(function() { callback(currTime + timeToCall); }, timeToCall);
-            lastTime = currTime + timeToCall;
-            return id;
-        };
-        returnVals.cancelAnimFrame = function(id){ clearTimeout(id); };
-    }
-
-    // requires an array or space separated string of css values to check against: e.g. ['boxSizing', 'WebkitBoxSizing'] or 'boxSizing WebkitBoxSizing'
-    // will return the first supported value in the sequence 
-    returnVals.cssCheck = function(valueArray){ return cssCheck(valueArray); };
-
-    // takes a string (which will be converted into a regular expression) to check against the userAgent e.g. 'firefox|chrome' will return true in both firefox and chrome
-    returnVals.uaCheck = function(userAgentValue){ return uaCheck(userAgentValue); };
 
     return returnVals;
 })();
